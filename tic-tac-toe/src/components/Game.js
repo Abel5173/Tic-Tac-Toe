74c4911ee722ae3266c8885ef2498a5b08b78bf3
Abel <abelzeleke5173@gmail.com>
2023-01-11 22:11:30 +0300

    renderMoves functionand jsx

--- tic-tac-toe/src/components/Game.js
+++ tic-tac-toe/src/components/Game.js
@@ -8,30 +8,45 @@ const style={
 };
 
 const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
-  const winner = calculateWinner(board);
+  const winner = calculateWinner(history[stepNumber]);
 
   const handleClick = (i) => {
-    const boardCopy = [...board];
+    const timeInHistory = history.slice(0, stepNumber+1);
+    const current = timeInHistory[stepNumber]
+    const squares = [...current];
     // if user click an occupied square or if game is won, return
-    if (winner || boardCopy[i]) return;
+    if (winner || squares[i]) return;
     //Put an X or an O in the clicked square
-    boardCopy[i] = xIsNext ? "X" : "O";
-    setBoard(boardCopy);
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory([...timeInHistory, squares]);
+    setStepNumber(timeInHistory.length)
     setXisNext(!xIsNext);
   };
 
-  const jumpTo = () => {};
+  const jumpTo = step => {
+    setStepNumber(step);
+    setXisNext(step% 2===0);
+  };
 
   const renderMoves = () => (
-    <button onClick={()=>setBoard(Array(9).fill(null))}>
-        Start Game
-    </button>
+    history.map((_step, move)=>{
+        const destination = move?`Go to move#${move}` : 'Go to start'
+        return (
+            <li key={move}>
+                <buttom onClick={()=> jumpTo(move)}>{destination}</buttom>
+            </li>
+        )
+    })
+    // <button onClick={()=>setBoard(Array(9).fill(null))}>
+    //     Start Game
+    // </button>
   );
   return (
     <>
-      <Board squares={board} onClick={handleClick} />
+      <Board squares={history[stepNumber]}onClick={handleClick} />
       <div style={style}>
         <p> {winner ? 'Winner: '+winner:'Next Player: '+(xIsNext?'X':'O')}</p>
         {renderMoves()}
